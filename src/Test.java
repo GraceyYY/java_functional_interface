760c23c1e1913392ad25098c521a6f7c02c4ab09
Grace <lunay0325@gmail.com>
2019-03-09 15:26:17 +0800

    Create instances of MathOperation by using lambda expression

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        MathOperation addition = (a, b) -> a + b;
+        MathOperation subtraction = (a, b) -> a - b;
+        MathOperation multiplication = (a, b) -> a * b;
+        MathOperation division = (a, b) -> a / b;
+
+
+    }
+}
