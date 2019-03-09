0de8843343b31ce6a465a899107d5cab571cf87f
Grace <lunay0325@gmail.com>
2019-03-09 15:38:34 +0800

    Edit the declaration of MathOperation instances

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,9 @@
 public class Test {
     public static void main(String[] args) {
-        MathOperation addition = (a, b) -> a + b;
-        MathOperation subtraction = (a, b) -> a - b;
-        MathOperation multiplication = (a, b) -> a * b;
-        MathOperation division = (a, b) -> a / b;
+        MathOperation<Integer> addition = (a, b) -> a + b;
+        MathOperation<Integer> subtraction = (a, b) -> a - b;
+        MathOperation<Integer> multiplication = (a, b) -> a * b;
+        MathOperation<Double> division = (a, b) -> a / (double)b;
 
         Integer a = 4;
         Integer b = 2;
